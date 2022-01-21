bea5914938290abcd575848885b83718acc97038
Angela <luzangelameloc1994@outlook.com>
2022-01-21 16:50:51 -0500

    Create Archivos_Abrir.java

---
+++ Archivos_Abrir/src/archivos_abrir/Archivos_Abrir.java
@@ -0,0 +1,11 @@
+package archivos_abrir;
+
+public class Archivos_Abrir {
+
+    public static void main(String[] args) {
+        //INSTANCIAR LA CLASE Archivos_Admon
+        Archivos_Admon archivo = new Archivos_Admon();
+        archivo.abrir();
+    }
+
+}
