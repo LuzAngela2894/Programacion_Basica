f1f49ec5221abff5086b30b8aa0988c88b86b920
Angela <luzangelameloc1994@outlook.com>
2022-01-21 18:41:33 -0500

    Create Medico.java

---
+++ Ejercicio_Medico_Paciente/src/ejercicio_medico_paciente/Medico.java
@@ -0,0 +1,31 @@
+package ejercicio_medico_paciente;
+
+public class Medico {
+    private String Cedula;
+    private String Nombre;
+    private Double Salario;
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Double getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(Double Salario) {
+        this.Salario = Salario;
+    }
+}
