a293ac6fcf403abdcfc91e25258a49ec9d47b042
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:38:03 -0500

    Create Reto3.java

---
+++ Reto3/src/reto3/Reto3.java
@@ -0,0 +1,13 @@
+package reto3;
+
+import java.io.IOException;
+import view.View;
+
+public class Reto3 {
+
+    public static void main(String[] args) throws IOException {
+        //INSTANCIAR LA CLASE View PARA VISUALIZAR
+        View objview = new View();
+        objview.menu();
+    }
+}
