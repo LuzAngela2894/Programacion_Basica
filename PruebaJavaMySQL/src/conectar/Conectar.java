80e1e921e3ffd3ef1789dafe8228262bb7491049
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:30:30 -0500

    Create Conectar.java

---
+++ PruebaJavaMySQL/src/conectar/Conectar.java
@@ -0,0 +1,22 @@
+package conectar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conectar {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/empresa";
+    public static final String USER = "root";
+    public static final String CLAVE = "root";
+    
+    public Connection getConexion(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(URL, USER, CLAVE);            
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return con;
+    }
+}
