f231801b373521c8d740d9a0672162ec5c6879c6
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:50:24 -0500

    Create ViewTest.java

---
+++ Reto4/test/view/ViewTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luz Angela Melo
+ */
+public class ViewTest {
+    
+    public ViewTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of menu method, of class View.
+     */
+    @Test
+    public void testMenu() throws Exception {
+        System.out.println("menu");
+        View instance = new View();
+        instance.menu();
+        // TODO review the generated test code and remove the default call to fail.
+    }
+    
+}
