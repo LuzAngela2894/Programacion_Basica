4e10cc23cd36ce59590bea4f7500385486b7088c
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:48:13 -0500

    Create type-search-index.js

---
+++ Reto4/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"conexion","l":"Conexion"},{"p":"controller","l":"EBO"},{"p":"dao","l":"EDAO"},{"p":"modelo","l":"EVO"},{"p":"dao","l":"IEDAO"},{"p":"reto4","l":"Reto4"},{"p":"view","l":"View"}]
