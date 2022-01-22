1fb6b7b7032128a2a598a91792cda4c79e306581
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:28:19 -0500

    Create EmpleadoVO.java

---
+++ Patron_DTO/src/VO/EmpleadoVO.java
@@ -0,0 +1,46 @@
+package VO;
+
+public class EmpleadoVO {
+
+    //ATRIBUTOS
+    private int id;
+    private String nombre;
+    private String apellido;
+
+    //CONSTRUCTOR DE LA CLASE EMPLEADOVO
+    public EmpleadoVO(int id, String nombre, String apellido) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    //GETTERS & SETTERS
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return this.getNombre() + " " + this.getApellido();
+    }
+}
