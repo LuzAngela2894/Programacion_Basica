957a3bb2650a2277b9e705a769ff9ab26f1117a7
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:20:13 -0500

    Create EjercicioRetoTest.java

---
+++ Ejercicio_Reto_SQL/test/main/EjercicioRetoTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luz Angela Melo
+ */
+public class EjercicioRetoTest {
+    
+    public EjercicioRetoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class EjercicioReto.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        EjercicioReto.main(args);
+    }
+}
