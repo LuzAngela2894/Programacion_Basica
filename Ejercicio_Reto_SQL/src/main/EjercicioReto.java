fa37d74c5a80e3f5cb294747cbdb03cfd0df4012
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:11:22 -0500

    Create EjercicioReto.java

---
+++ Ejercicio_Reto_SQL/src/main/EjercicioReto.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.io.IOException;
+import vista.Vista;
+
+public class EjercicioReto {
+
+    public static void main(String[] args) throws IOException {
+        //INSTANCIAR LA CLASE VISTA PARA VISUALIZAR
+        Vista objvista = new Vista();
+        objvista.menu();
+    }
+}
