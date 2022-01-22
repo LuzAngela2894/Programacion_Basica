09e4e792fa790894f7cfc5a6d38be1bc24c66030
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:24:11 -0500

    Create DocenteTC.java

---
+++ Herencia_Docente/src/herenciadocente/DocenteTC.java
@@ -0,0 +1,15 @@
+package herenciadocente;
+
+public class DocenteTC extends Docente {
+
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
