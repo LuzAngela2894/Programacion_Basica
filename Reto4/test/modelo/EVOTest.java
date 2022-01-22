c6e258f96a01b22ce05e016a78584321c737239c
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:50:14 -0500

    Create EVOTest.java

---
+++ Reto4/test/modelo/EVOTest.java
@@ -0,0 +1,256 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luz Angela Melo
+ */
+public class EVOTest {
+    
+    public EVOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNombres method, of class EVO.
+     */
+    @Test
+    public void testGetNombres() {
+        System.out.println("getNombres");
+        EVO instance = new EVO();
+        String expResult = "";
+        String result = instance.getNombres();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setNombres method, of class EVO.
+     */
+    @Test
+    public void testSetNombres() {
+        System.out.println("setNombres");
+        String nombres = "";
+        EVO instance = new EVO();
+        instance.setNombres(nombres);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getApellidos method, of class EVO.
+     */
+    @Test
+    public void testGetApellidos() {
+        System.out.println("getApellidos");
+        EVO instance = new EVO();
+        String expResult = "";
+        String result = instance.getApellidos();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setApellidos method, of class EVO.
+     */
+    @Test
+    public void testSetApellidos() {
+        System.out.println("setApellidos");
+        String apellidos = "";
+        EVO instance = new EVO();
+        instance.setApellidos(apellidos);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getFnacimiento method, of class EVO.
+     */
+    @Test
+    public void testGetFnacimiento() {
+        System.out.println("getFnacimiento");
+        EVO instance = new EVO();
+        String expResult = "";
+        String result = instance.getFnacimiento();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setFnacimiento method, of class EVO.
+     */
+    @Test
+    public void testSetFnacimiento() {
+        System.out.println("setFnacimiento");
+        String fnacimiento = "";
+        EVO instance = new EVO();
+        instance.setFnacimiento(fnacimiento);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getCinstitucional method, of class EVO.
+     */
+    @Test
+    public void testGetCinstitucional() {
+        System.out.println("getCinstitucional");
+        EVO instance = new EVO();
+        String expResult = "";
+        String result = instance.getCinstitucional();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setCinstitucional method, of class EVO.
+     */
+    @Test
+    public void testSetCinstitucional() {
+        System.out.println("setCinstitucional");
+        String cinstitucional = "";
+        EVO instance = new EVO();
+        instance.setCinstitucional(cinstitucional);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getCpersonal method, of class EVO.
+     */
+    @Test
+    public void testGetCpersonal() {
+        System.out.println("getCpersonal");
+        EVO instance = new EVO();
+        String expResult = "";
+        String result = instance.getCpersonal();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setCpersonal method, of class EVO.
+     */
+    @Test
+    public void testSetCpersonal() {
+        System.out.println("setCpersonal");
+        String cpersonal = "";
+        EVO instance = new EVO();
+        instance.setCpersonal(cpersonal);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getNcelular method, of class EVO.
+     */
+    @Test
+    public void testGetNcelular() {
+        System.out.println("getNcelular");
+        EVO instance = new EVO();
+        long expResult = 0L;
+        long result = instance.getNcelular();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setNcelular method, of class EVO.
+     */
+    @Test
+    public void testSetNcelular() {
+        System.out.println("setNcelular");
+        long ncelular = 0L;
+        EVO instance = new EVO();
+        instance.setNcelular(ncelular);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getNfijo method, of class EVO.
+     */
+    @Test
+    public void testGetNfijo() {
+        System.out.println("getNfijo");
+        EVO instance = new EVO();
+        long expResult = 0L;
+        long result = instance.getNfijo();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setNfijo method, of class EVO.
+     */
+    @Test
+    public void testSetNfijo() {
+        System.out.println("setNfijo");
+        long nfijo = 0L;
+        EVO instance = new EVO();
+        instance.setNfijo(nfijo);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of getPrograma method, of class EVO.
+     */
+    @Test
+    public void testGetPrograma() {
+        System.out.println("getPrograma");
+        EVO instance = new EVO();
+        String expResult = "";
+        String result = instance.getPrograma();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+
+    /**
+     * Test of setPrograma method, of class EVO.
+     */
+    @Test
+    public void testSetPrograma() {
+        System.out.println("setPrograma");
+        String programa = "";
+        EVO instance = new EVO();
+        instance.setPrograma(programa);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
