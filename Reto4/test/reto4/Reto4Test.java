a2dcf9de86f86fc1252ee96b491d3596c452eeac
Angela <luzangelameloc1994@outlook.com>
2022-01-21 22:50:21 -0500

    Create Reto4Test.java

---
+++ Reto4/test/reto4/Reto4Test.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto4;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luz Angela Melo
+ */
+public class Reto4Test {
+    
+    public Reto4Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Reto4.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        Reto4.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
