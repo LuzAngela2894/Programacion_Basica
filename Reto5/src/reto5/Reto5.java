627b7aef2e4ac5cb085a7e314ecbd0a497fd4e80
Angela <luzangelameloc1994@outlook.com>
2022-01-21 23:06:00 -0500

    Create Reto5.java

---
+++ Reto5/src/reto5/Reto5.java
@@ -0,0 +1,13 @@
+package reto5;
+
+import java.io.IOException;
+import view.View;
+
+public class Reto5 {
+
+    public static void main(String[] args) throws IOException {
+        View objetoview = new View();
+        objetoview.setVisible(true);
+        objetoview.setLocationRelativeTo(null);
+    }
+}
